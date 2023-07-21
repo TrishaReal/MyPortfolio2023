ba59a5a51dd43c07e63c84fa9491bed470d574f4
Trisha Sairene Real <trisha.real@softfobia.com>
2023-07-21 17:33:44 +0200

    - custom css path edited

--- controllers/getInit.js
+++ controllers/getInit.js
@@ -5,17 +5,13 @@ const getInit = async () => {
   const Loader = (folder) => {
     const folderPath = folder?.replace(/\\/g, "/");
 
-    console.log("@@@@@-->", folderPath);
+    console.log("----> ", folderPath);
 
     const cssFiles = fs?.readdirSync(folderPath);
-
-    console.log("###### --->", cssFiles)
-
-    const relativePath = folderPath?.split("public")[1];
-    const cssPath = cssFiles.map((filename) => {
-      console.log("PATH --->", relativePath)
-      return path.join(folderPath, filename)
-    });
+    const relativePath = folderPath?.split("public/")[1];
+    const cssPath = cssFiles.map((filename) =>
+      path.join(relativePath, filename)
+    );
     return [...cssPath];
   };
 
