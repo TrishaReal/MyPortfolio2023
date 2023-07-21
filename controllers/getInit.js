5afb6b3ae318ff363d2c324be816763e33568493
Trisha Sairene Real <trisha.real@softfobia.com>
2023-07-21 17:21:55 +0200

    - custom css path edited

--- controllers/getInit.js
+++ controllers/getInit.js
@@ -19,8 +19,8 @@ const getInit = async () => {
     return [...cssPath];
   };
 
-  const styles = Loader("assets/css/");
-  const scripts = Loader("assets/js/");
+  const styles = Loader("/public/assets/css/");
+  const scripts = Loader("/public/assets/js/");
 
   return {
     styles: styles,
